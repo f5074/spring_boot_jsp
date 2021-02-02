bc749f2457865b2a1417f0f28a6735cbbf100b51
f5074 <oseongryu@gmail.com>
2021-02-02 14:21:28 +0900

    First Commit

---
+++ src/test/java/com/tistory/f5074/spring_boot_jsp/SpringBootJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.f5074.spring_boot_jsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
