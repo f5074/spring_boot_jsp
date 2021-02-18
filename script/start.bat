b832425b51ae29667ac2f03b0cb905ce0aed8383
f5074 <oseongryu@gmail.com>
2021-02-18 10:58:57 +0900

    [Script]] -

--- script/start.bat
+++ script/start.bat
@@ -1,1 +1,1 @@
-java -jar spring_boot_jsp.war --spring.profiles.active=prod --spring.datasource.username=drawing  --spring.datasource.password=drawing --variables.ip=localhost --variables.sid=XE
+java -jar spring_boot_jsp.war --spring.profiles.active=prod --spring.datasource.username=drawing  --spring.datasource.password=drawing --variables.ip=localhost --variables.sid=XE server.port=8080
